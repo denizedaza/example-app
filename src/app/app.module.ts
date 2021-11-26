d7f1bda63c9862d4fbd6eb087f9c75801a8cdc49
denizedaza <dazadenize@gmail.com>
2021-11-26 16:48:24 -0500

    add warning and success alerts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningComponent } from './warning-alert/warning.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
